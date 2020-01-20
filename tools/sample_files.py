f46d7c4ad181e389fc9be2a48f01d355fb8bc770
Jackson Fellows <jtfellows@icloud.com>
2020-01-20 15:23:01 -0800

    seek around instead of reading all of the lines at once

--- tools/sample_files.py
+++ tools/sample_files.py
@@ -5,19 +5,30 @@ import random
 import re
 
 def sample_file(f, lim, ignore_header = False):
-    # assumes every line ends in a newline (inc. last line)
     if ignore_header:
         f.readline()
-    lines = f.readlines()
+    start = f.tell()
+    f.seek(0, 2)
+    f_len = f.tell()
+
+    if f_len == 0:
+        return 0
+
     used = 0
-    while len(lines) > 0:
-        i = random.randrange(0, len(lines))
-        line = lines[i]
-        if used + len(line) > lim:
+    while 1:
+        pos = random.randrange(start, f_len)
+        f.seek(pos)
+        # backtrack to last newline
+        while pos > start and f.read(1) != b'\n':
+            pos -= 1
+            f.seek(pos)
+        line = f.readline()
+        if used + len(line) < lim:
+            os.write(1, line)
+            used += len(line)
+        else:
             break
-        os.write(1, line)
-        used += len(line)
-        del lines[i]
+
     return used
 
 def sample_files(files, lim, ignore_headers = False):
